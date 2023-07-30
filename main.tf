94bd22e3de4f25c5691fa65bd5fb9a78aab282cf
electronff <aminumuritala@gmail.com>
2023-07-29 23:31:26 -0500

    apply

--- main.tf
+++ main.tf
@@ -8,15 +8,25 @@ resource "aws_instance" "jenkins" {
 
   user_data = <<-EOF
                 #!/bin/bash
-                sudo apt update
-                sudo apt install openjdk-17-jre -y
-                curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee \
-                  /usr/share/keyrings/jenkins-keyring.asc > /dev/null
-                echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
-                  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
-                  /etc/apt/sources.list.d/jenkins.list > /dev/null
-                sudo apt-get update
-                sudo apt-get install jenkins
+                sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+                sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+                sudo yum upgrade -y
+                sudo yum install java-17-openjdk -y
+                sudo yum install jenkins -y
+                sudo systemctl start jenkins
+                sudo systemctl enable jenkins
+                # The following commands set up firewall rules for Jenkins.
+                # Substitute 'YOURPORT' with the port number you want Jenkins to run on.
+                YOURPORT=8080
+                PERM="--permanent"
+                SERV="\${PERM} --service=jenkins"
+                sudo firewall-cmd \${PERM} --new-service=jenkins
+                sudo firewall-cmd \${SERV} --set-short="Jenkins ports"
+                sudo firewall-cmd \${SERV} --set-description="Jenkins port exceptions"
+                sudo firewall-cmd \${SERV} --add-port=\${YOURPORT}/tcp
+                sudo firewall-cmd \${PERM} --add-service=jenkins
+                sudo firewall-cmd --zone=public --add-service=http --permanent
+                sudo firewall-cmd --reload
               EOF
 
   tags = {
@@ -33,10 +43,11 @@ resource "aws_instance" "tomcat" {
 
   vpc_security_group_ids = [aws_default_security_group.default.id]
 
+
   user_data = <<-EOF
                 #!/bin/bash
-                sudo apt update
-                sudo apt install openjdk-17-jre -y
+                sudo yum update -y
+                sudo yum install -y java-1.8.0-openjdk
                 wget https://downloads.apache.org/tomcat/tomcat-9/v9.0.41/bin/apache-tomcat-9.0.41.tar.gz
                 tar -xvf apache-tomcat-9.0.41.tar.gz
                 sudo mv apache-tomcat-9.0.41 /usr/local/tomcat9
@@ -61,7 +72,7 @@ resource "aws_instance" "tomcat" {
                 WantedBy=multi-user.target
                 sudo systemctl daemon-reload
                 sudo systemctl start tomcat
-              EOF
+                EOF
 
   tags = {
     Name = "tomcat"
@@ -78,22 +89,21 @@ resource "aws_instance" "sonarqube" {
 
   user_data = <<-EOF
               #!/bin/bash
-              sudo apt-get update -y
-              sudo apt-get install -y openjdk-11-jdk wget unzip
+              sudo yum update -y
+              sudo yum install -y java-11-openjdk wget unzip
               wget https://binaries.sonarsource.com/Distribution/sonarqube/sonarqube-7.9.5.zip
               unzip sonarqube-7.9.5.zip
               sudo mv sonarqube-7.9.5 /opt/sonarqube
               sudo bash -c "echo 'sonar.jdbc.username=sonar' >> /opt/sonarqube/conf/sonar.properties"
               sudo bash -c "echo 'sonar.jdbc.password=sonar' >> /opt/sonarqube/conf/sonar.properties"
               sudo bash -c "echo 'sonar.jdbc.url=jdbc:postgresql://localhost/sonarqube' >> /opt/sonarqube/conf/sonar.properties"
-              sudo bash -c "echo 'sonar.web.javaAdditionalOpts=-server' >> /opt/sonarqube/conf/sonar.properties"
               sudo adduser sonar
               sudo chown -R sonar:sonar /opt/sonarqube
               sudo bash -c 'echo "sonar   -   nofile   65536" >> /etc/security/limits.d/99-sonarqube.conf'
               sudo bash -c 'echo "sonar   -   nproc    4096" >> /etc/security/limits.d/99-sonarqube.conf'
               sudo bash -c 'echo "vm.max_map_count=262144" >> /etc/sysctl.d/99-sonarqube.conf'
               sudo sysctl --system
-              sudo su - sonar -c "/opt/sonarqube/bin/linux-x86-64/sonar.sh start"
+              sudo systemctl start sonarqube
               EOF
 
   tags = {
