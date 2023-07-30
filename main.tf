955cde74914064e62c42cb34f9a2b7d5002d44d8
electronff <aminumuritala@gmail.com>
2023-07-29 23:35:47 -0500

    apply

--- main.tf
+++ main.tf
@@ -19,12 +19,12 @@ resource "aws_instance" "jenkins" {
                 # Substitute 'YOURPORT' with the port number you want Jenkins to run on.
                 YOURPORT=8080
                 PERM="--permanent"
-                SERV="\${PERM} --service=jenkins"
-                sudo firewall-cmd \${PERM} --new-service=jenkins
-                sudo firewall-cmd \${SERV} --set-short="Jenkins ports"
-                sudo firewall-cmd \${SERV} --set-description="Jenkins port exceptions"
-                sudo firewall-cmd \${SERV} --add-port=\${YOURPORT}/tcp
-                sudo firewall-cmd \${PERM} --add-service=jenkins
+                SERV="\$\{PERM} --service=jenkins"
+                sudo firewall-cmd \$\{PERM} --new-service=jenkins
+                sudo firewall-cmd \$\{SERV} --set-short="Jenkins ports"
+                sudo firewall-cmd \$\{SERV} --set-description="Jenkins port exceptions"
+                sudo firewall-cmd \$\{SERV} --add-port=\$\{YOURPORT}/tcp
+                sudo firewall-cmd \$\{PERM} --add-service=jenkins
                 sudo firewall-cmd --zone=public --add-service=http --permanent
                 sudo firewall-cmd --reload
               EOF
