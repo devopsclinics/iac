a3063f12a86fe489eb8c38a3052b4d8f79be82e0
electronff <aminumuritala@gmail.com>
2023-07-30 11:22:23 -0500

    test git

--- main.tf
+++ main.tf
@@ -36,79 +36,79 @@ resource "aws_instance" "jenkins" {
 
 
 
-resource "aws_instance" "tomcat" {
-  ami           = var.web_server_ami
-  instance_type = var.web_server_instance_type
-  key_name      = "MyKeyPair"
+# resource "aws_instance" "tomcat" {
+#   ami           = var.web_server_ami
+#   instance_type = var.web_server_instance_type
+#   key_name      = "MyKeyPair"
 
-  vpc_security_group_ids = [aws_default_security_group.default.id]
-
-
-  user_data = <<-EOF
-                #!/bin/bash
-                sudo yum update -y
-                sudo yum install -y java-1.8.0-openjdk
-                wget https://downloads.apache.org/tomcat/tomcat-9/v9.0.41/bin/apache-tomcat-9.0.41.tar.gz
-                tar -xvf apache-tomcat-9.0.41.tar.gz
-                sudo mv apache-tomcat-9.0.41 /usr/local/tomcat9
-                sudo useradd -r tomcat
-                sudo chown -R tomcat:tomcat /usr/local/tomcat9
-                sudo tee /etc/systemd/system/tomcat.service<<EOF
-                [Unit]
-                Description=Tomcat Server
-                After=syslog.target network.target
-                [Service]
-                Type=forking
-                User=tomcat
-                Group=tomcat
-                Environment=CATALINA_HOME=/usr/local/tomcat9
-                Environment=CATALINA_BASE=/usr/local/tomcat9
-                Environment=CATALINA_PID=/usr/local/tomcat9/temp/tomcat.pid
-                ExecStart=/usr/local/tomcat9/bin/catalina.sh start
-                ExecStop=/usr/local/tomcat9/bin/catalina.sh stop
-                RestartSec=12
-                Restart=always
-                [Install]
-                WantedBy=multi-user.target
-                sudo systemctl daemon-reload
-                sudo systemctl start tomcat
-                EOF
-
-  tags = {
-    Name = "tomcat"
-  }
-}
+#   vpc_security_group_ids = [aws_default_security_group.default.id]
 
 
-resource "aws_instance" "sonarqube" {
-  ami           = var.web_server_ami
-  instance_type = var.web_server_instance_type
-  key_name      = "MyKeyPair"
+#   user_data = <<-EOF
+#                 #!/bin/bash
+#                 sudo yum update -y
+#                 sudo yum install -y java-1.8.0-openjdk
+#                 wget https://downloads.apache.org/tomcat/tomcat-9/v9.0.41/bin/apache-tomcat-9.0.41.tar.gz
+#                 tar -xvf apache-tomcat-9.0.41.tar.gz
+#                 sudo mv apache-tomcat-9.0.41 /usr/local/tomcat9
+#                 sudo useradd -r tomcat
+#                 sudo chown -R tomcat:tomcat /usr/local/tomcat9
+#                 sudo tee /etc/systemd/system/tomcat.service<<EOF
+#                 [Unit]
+#                 Description=Tomcat Server
+#                 After=syslog.target network.target
+#                 [Service]
+#                 Type=forking
+#                 User=tomcat
+#                 Group=tomcat
+#                 Environment=CATALINA_HOME=/usr/local/tomcat9
+#                 Environment=CATALINA_BASE=/usr/local/tomcat9
+#                 Environment=CATALINA_PID=/usr/local/tomcat9/temp/tomcat.pid
+#                 ExecStart=/usr/local/tomcat9/bin/catalina.sh start
+#                 ExecStop=/usr/local/tomcat9/bin/catalina.sh stop
+#                 RestartSec=12
+#                 Restart=always
+#                 [Install]
+#                 WantedBy=multi-user.target
+#                 sudo systemctl daemon-reload
+#                 sudo systemctl start tomcat
+#                 EOF
 
-  vpc_security_group_ids = [aws_default_security_group.default.id]
+#   tags = {
+#     Name = "tomcat"
+#   }
+# }
 
-  user_data = <<-EOF
-              #!/bin/bash
-              sudo yum update -y
-              sudo yum install -y java-11-openjdk wget unzip
-              wget https://binaries.sonarsource.com/Distribution/sonarqube/sonarqube-7.9.5.zip
-              unzip sonarqube-7.9.5.zip
-              sudo mv sonarqube-7.9.5 /opt/sonarqube
-              sudo bash -c "echo 'sonar.jdbc.username=sonar' >> /opt/sonarqube/conf/sonar.properties"
-              sudo bash -c "echo 'sonar.jdbc.password=sonar' >> /opt/sonarqube/conf/sonar.properties"
-              sudo bash -c "echo 'sonar.jdbc.url=jdbc:postgresql://localhost/sonarqube' >> /opt/sonarqube/conf/sonar.properties"
-              sudo adduser sonar
-              sudo chown -R sonar:sonar /opt/sonarqube
-              sudo bash -c 'echo "sonar   -   nofile   65536" >> /etc/security/limits.d/99-sonarqube.conf'
-              sudo bash -c 'echo "sonar   -   nproc    4096" >> /etc/security/limits.d/99-sonarqube.conf'
-              sudo bash -c 'echo "vm.max_map_count=262144" >> /etc/sysctl.d/99-sonarqube.conf'
-              sudo sysctl --system
-              sudo systemctl start sonarqube
-              EOF
 
-  tags = {
-    Name = "SonarQube-Server"
-  }
-}
+# resource "aws_instance" "sonarqube" {
+#   ami           = var.web_server_ami
+#   instance_type = var.web_server_instance_type
+#   key_name      = "MyKeyPair"
+
+#   vpc_security_group_ids = [aws_default_security_group.default.id]
+
+#   user_data = <<-EOF
+#               #!/bin/bash
+#               sudo yum update -y
+#               sudo yum install -y java-11-openjdk wget unzip
+#               wget https://binaries.sonarsource.com/Distribution/sonarqube/sonarqube-7.9.5.zip
+#               unzip sonarqube-7.9.5.zip
+#               sudo mv sonarqube-7.9.5 /opt/sonarqube
+#               sudo bash -c "echo 'sonar.jdbc.username=sonar' >> /opt/sonarqube/conf/sonar.properties"
+#               sudo bash -c "echo 'sonar.jdbc.password=sonar' >> /opt/sonarqube/conf/sonar.properties"
+#               sudo bash -c "echo 'sonar.jdbc.url=jdbc:postgresql://localhost/sonarqube' >> /opt/sonarqube/conf/sonar.properties"
+#               sudo adduser sonar
+#               sudo chown -R sonar:sonar /opt/sonarqube
+#               sudo bash -c 'echo "sonar   -   nofile   65536" >> /etc/security/limits.d/99-sonarqube.conf'
+#               sudo bash -c 'echo "sonar   -   nproc    4096" >> /etc/security/limits.d/99-sonarqube.conf'
+#               sudo bash -c 'echo "vm.max_map_count=262144" >> /etc/sysctl.d/99-sonarqube.conf'
+#               sudo sysctl --system
+#               sudo systemctl start sonarqube
+#               EOF
+
+#   tags = {
+#     Name = "SonarQube-Server"
+#   }
+# }
 
 
--- output.tf
+++ output.tf
@@ -1,12 +1,12 @@
-output "sonarqube_public_ip" {
-  value       = aws_instance.sonarqube.public_ip
-  description = "Public IP of the SonarQube instance"
-}
+# output "sonarqube_public_ip" {
+#   value       = aws_instance.sonarqube.public_ip
+#   description = "Public IP of the SonarQube instance"
+# }
 
-output "sonarqube_public_dns" {
-  value       = aws_instance.sonarqube.public_dns
-  description = "Public DNS of the SonarQube instance"
-}
+# output "sonarqube_public_dns" {
+#   value       = aws_instance.sonarqube.public_dns
+#   description = "Public DNS of the SonarQube instance"
+# }
 
 output "jenkins_public_ip" {
   value       = aws_instance.jenkins.public_ip
@@ -18,12 +18,12 @@ output "jenkins_public_dns" {
   description = "Public DNS of the Jenkins instance"
 }
 
-output "tomcat_public_ip" {
-  value       = aws_instance.tomcat.public_ip
-  description = "Public IP of the Tomcat instance"
-}
+# output "tomcat_public_ip" {
+#   value       = aws_instance.tomcat.public_ip
+#   description = "Public IP of the Tomcat instance"
+# }
 
-output "tomcat_public_dns" {
-  value       = aws_instance.tomcat.public_dns
-  description = "Public DNS of the Tomcat instance"
-}
+# output "tomcat_public_dns" {
+#   value       = aws_instance.tomcat.public_dns
+#   description = "Public DNS of the Tomcat instance"
+# }
